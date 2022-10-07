c137d95db58a6132bb8b3680de2a17d850c59b1d
Funky Panda <72228432+Funky-Panda@users.noreply.github.com>
2022-10-07 17:36:59 +0100

    Add files via upload

---
+++ Switch.ino
@@ -0,0 +1,125 @@
+#ifdef ENABLE_DEBUG
+       #define DEBUG_ESP_PORT Serial
+       #define NODEBUG_WEBSOCKETS
+       #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#ifdef ESP8266 
+       #include <ESP8266WiFi.h>
+#endif 
+#ifdef ESP32   
+       #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+#include <Stepper.h>
+#define WIFI_SSID         ""    
+#define WIFI_PASS         ""
+#define APP_KEY           ""      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        ""   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+#define SWITCH_ID         ""    // Should look like "5dc1564130xxxxxxxxxxxxxx"
+#define BAUD_RATE         9600                // Change baudrate to your need
+
+#define BUTTON_PIN 0 
+#define LED_PIN   2  
+
+bool myPowerState = false;
+unsigned long lastBtnPress = 0;
+
+
+int outPin = 23;
+
+#define STEPS 200
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(STEPS, 18, 12, 19, 13);
+/* bool onPowerState(String deviceId, bool &state) 
+ *
+ * Callback for setPowerState request
+ * parameters
+ *  String deviceId (r)
+ *    contains deviceId (useful if this callback used by multiple devices)
+ *  bool &state (r/w)
+ *    contains the requested state (true:on / false:off)
+ *    must return the new state
+ * 
+ * return
+ *  true if request should be marked as handled correctly / false if not
+ */
+bool onPowerState(const String &deviceId, bool &state) {
+    if (state) {
+        digitalWrite(outPin, HIGH);
+         Serial.printf("moving forward -4500 steps");
+        myStepper.step(300*160);
+        digitalWrite(outPin, LOW);
+    } else {
+        digitalWrite(outPin, HIGH);
+        Serial.printf("moving back 4500 steps");
+        myStepper.step(-300*160);
+        digitalWrite(outPin, LOW);
+    }
+    return true;
+}
+
+void handleButtonPress() {
+  unsigned long actualMillis = millis(); // get actual millis() and keep it in variable actualMillis
+  if (digitalRead(BUTTON_PIN) == LOW && actualMillis - lastBtnPress > 1000)  { // is button pressed (inverted logic! button pressed = LOW) and debounced?
+    if (myPowerState) {     // flip myPowerState: if it was true, set it to false, vice versa
+      myPowerState = false;
+    } else {
+      myPowerState = true;
+    }
+    digitalWrite(LED_PIN, myPowerState?LOW:HIGH); // if myPowerState indicates device turned on: turn on led (builtin led uses inverted logic: LOW = LED ON / HIGH = LED OFF)
+
+    // get Switch device back
+    SinricProSwitch& mySwitch = SinricPro[SWITCH_ID];
+    // send powerstate event
+    mySwitch.sendPowerStateEvent(myPowerState); // send the new powerState to SinricPro server
+    Serial.printf("Device %s turned %s (manually via flashbutton)\r\n", mySwitch.getDeviceId().c_str(), myPowerState?"on":"off");
+
+    lastBtnPress = actualMillis;  // update last button press variable
+  } 
+}
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add device to SinricPro
+  SinricProSwitch& mySwitch = SinricPro[SWITCH_ID];
+
+  // set callback function to device
+  mySwitch.onPowerState(onPowerState);
+
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+  //SinricPro.restoreDeviceStates(true); // Uncomment to restore the last known state from the server.
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  myStepper.setSpeed(60);
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, LOW);
+}
+
+void loop() {
+  handleButtonPress();
+  SinricPro.handle();
+}
